9ea0be054591b71c041db1dc6b75d85cdd8966ee
shenghaishxt <shenghaishxt@gmail.com>
2019-09-14 21:31:57 +0800

    0020.Valid Parentheses

--- Py/easy/0026.remove-duplicates-from-sorted-array.py
+++ Py/easy/0026.remove-duplicates-from-sorted-array.py
@@ -4,4 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        low = 0
+        for fast in range(1, len(nums)):
+            if nums[low] == nums[fast]:
+                fast += 1
+            else:
+                low += 1
+                nums[low] = nums[fast]
+        return low+1
+
+
+if __name__ == '__main__':
+    Sol = Solution()
+    print(Sol.removeDuplicates([1,1,2,3,4,5,5]))
