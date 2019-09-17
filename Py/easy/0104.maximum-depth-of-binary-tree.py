030f6b0a88a770221fafbd4081347687e702a2c7
shenghaishxt <shenghaishxt@gmail.com>
2019-09-17 22:40:51 +0800

    0104.maximum-depth-of-binary-tree

---
+++ Py/easy/0104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        ldep = self.maxDepth(root.left)
+        rdep = self.maxDepth(root.right)
+        return max(ldep, rdep) + 1
+
+
+if __name__ == '__main__':
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    Sol = Solution()
+    print(Sol.maxDepth(root))
