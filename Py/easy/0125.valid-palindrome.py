95c6d7f5e2b5df689a29689821c3558c454a10cd
shenghaishxt <shenghaishxt@gmail.com>
2019-09-22 21:05:13 +0800

    0125.valid-palindrome

--- Py/easy/0125.valid-palindrome.py
+++ Py/easy/0125.valid-palindrome.py
@@ -4,3 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        # 先处理字符串，然后使用头尾指针
+        s = ''.join(i for i in s if i.isalnum()).lower()
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+
+
+if __name__ == '__main__':
+    Sol = Solution()
+    str = "race a car"
+    print(Sol.isPalindrome(str))
