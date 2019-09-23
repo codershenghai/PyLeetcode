7a2686728652f910852dcc0c33e99fa66d644cdf
shenghaishxt <shenghaishxt@gmail.com>
2019-09-23 08:46:27 +0800

    0155.min-stack

---
+++ Py/easy/0155.min-stack.py
@@ -0,0 +1,50 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.data = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.data.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.data.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.data[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return min(self.data)
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+if __name__ == '__main__':
+    obj = MinStack()
+    obj.push(-2)
+    obj.push(0)
+    obj.push(-3)
+    print(obj.getMin())
+    obj.pop()
+    print(obj.top())
+    print(obj.getMin())
+    a = [1,2,3,4]
+    print(a.pop())
+    print(a)
