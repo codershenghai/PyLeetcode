9f02b36517d749c62cab7d3878eb91ffdc2ce77b
shenghaishxt <shenghaishxt@gmail.com>
2019-10-10 11:39:24 +0800

    改为从Utils中添加树的定义

--- Py/Utils/Definition.py
+++ Py/Utils/Definition.py
@@ -3,3 +3,11 @@ class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
