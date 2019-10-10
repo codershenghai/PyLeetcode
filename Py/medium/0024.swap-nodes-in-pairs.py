4aade200dac12932cc280ab7e0a1935430746f2f
shenghaishxt <shenghaishxt@gmail.com>
2019-10-10 17:02:44 +0800

    递归和循环

--- Py/medium/0024.swap-nodes-in-pairs.py
+++ Py/medium/0024.swap-nodes-in-pairs.py
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     head.next = ListNode(2)
     head.next.next = ListNode(3)
     head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
     p = head
     while p:
         print(p.val)
